84de8b0b838789f75fb88dc7494ae9e26ba7b857
Adam Novak <interfect@gmail.com>
2024-03-04 20:59:14 -0500

    Add an at sign

--- main.py
+++ main.py
@@ -22,6 +22,13 @@ class GameState:
         """
         raise NotImplementedError()
         
+        
+class Player:
+    def __init__(self):
+        self.x = 0
+        self.y = 0
+        self.symbol = "@"
+        
 class PlayingState(GameState):
     """
     State for playing the game.
@@ -29,12 +36,25 @@ class PlayingState(GameState):
     Walk around as an @.
     """
     
+    def __init__(self):
+        self.player = Player()
+    
     def render_to(self, console: tcod.console.Console):
+        console.clear()
         console.draw_frame(0, 0, console.width, console.height, "Super RPG 640x480")
         console.print(1, 1, "Hello World")
         
+        # Find where to put the view upper left corner to center the player
+        view_x = self.player.x - console.width // 2
+        view_y = self.player.y - console.height // 2
+        
+        for to_render in [self.player]:
+            console.print(to_render.x - view_x, to_render.y - view_y, to_render.symbol)
+        
     def handle_event(self, event: tcod.event.Event):
         pass
+        
+
 
 def force_min_size(context: tcod.context.Context) -> None:
     """
